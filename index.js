3b34f27798d6038b0e3fa0e044bde95009e98d2f
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-09-18 00:34:36 +0300

    First Trial

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+return gifts;
+}
+
+const nameList = ["Eva", "Paul", "Barbara"];
+const greetings = [];
+function writeCards(arr, event){
+for (let x = 0; x < arr.length; x++) {
+    greetings.push(`Thank you, ${arr[x]}, for the wonderful ${event} gift!`);
+}
+return greetings;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
